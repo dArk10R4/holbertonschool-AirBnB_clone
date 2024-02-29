7e0e4170ed7c5018918d12b89338346537aefc31
Kamalusta <kamalusta@gmail.com>
2024-02-29 14:06:14 +0400

    first task

---
+++ models/base_model.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''doc'''
+
+from datetime import datetime
+import uuid
+
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        self.__dict__['__class__'] = self.__class__.__name__
+        # updated_at.isoformat()
+        # updated_at.isoformat()
+        return self.__dict__
