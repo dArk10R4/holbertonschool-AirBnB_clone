f86b2f847b582eee5c2bcc30f1a155e02ca73998
Kamalusta <kamalusta@gmail.com>
2024-03-01 21:58:22 +0400

    correction

--- models/base_model.py
+++ models/base_model.py
@@ -17,10 +17,10 @@ class BaseModel:
                                                 "%Y-%m-%dT%H:%M:%S.%f")
             self.updated_at = datetime.strptime(kwargs['updated_at'],
                                                 "%Y-%m-%dT%H:%M:%S.%f")
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         '''String of BaseModel'''
