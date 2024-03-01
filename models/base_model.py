9be2366a811a69e5e2c9995471ca1a7100a64f3b
Kamalusta <kamalusta@gmail.com>
2024-03-01 21:44:31 +0400

    documentation

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,11 @@ import uuid
 
 
 class BaseModel:
+    '''Base class'''
+    
     def __init__(self, *args, **kwargs):
+        '''Initializer'''
+        
         if kwargs:
             self.id = kwargs['id']
             self.created_at = datetime.strptime(kwargs['created_at'],
@@ -19,14 +23,20 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def __str__(self):
+        '''String of BaseModel'''
+        
         return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
+        '''update updated tiem'''
+        
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
+        '''Dictionary represendation'''
+        
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
