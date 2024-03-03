120f396d5f765bf76fddc951ad62b0f3394b5b15
dArk10R4 <goylerov04@gmail.com>
2024-03-03 15:09:33 +0400

    8

--- models/__init__.py
+++ models/__init__.py
@@ -8,9 +8,7 @@ from models.base_model import BaseModel
 from models.user import User
 
 from models.engine.file_storage import FileStorage
+from models.engine.file_storage import classes
 storage = FileStorage()
 storage.reload()
 
-classes = {"BaseModel": BaseModel, 
-           "User": User
-           }
