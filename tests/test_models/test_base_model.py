f3ff6f67d4f967ce55949599444f590e2cf745ec
dArk10R4 <goylerov04@gmail.com>
2024-03-03 16:57:31 +0400

    add tests for base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,6 +5,7 @@
 from models.base_model import BaseModel
 import unittest
 import datetime
+import time
 
 class TestBaseModel(unittest.TestCase):
     base = None
@@ -19,25 +20,27 @@ class TestBaseModel(unittest.TestCase):
         base = BaseModel()
         self.assertIsInstance(base, BaseModel)
         self.assertIsInstance(base.id, str)
-        self.assertIsInstance(base.created_at, datetime)
-        self.assertIsInstance(base.updated_at, datetime)
+        self.assertIsInstance(base.created_at, datetime.datetime)
+        self.assertIsInstance(base.updated_at, datetime.datetime)
         del base
 
     def test_id(self):
-        self.assertEqual(self.base.id, "")
+        self.assertNotEqual(self.base.id, "")
         self.base.id = "123"
         self.assertEqual(self.base.id, "123")
 
     def test_created_at(self):
-        self.assertIsInstance(self.base.created_at, datetime)
+        self.assertIsInstance(self.base.created_at, datetime.datetime)
 
     def test_updated_at(self):
-        self.assertIsInstance(self.base.updated_at, datetime)
+        self.assertIsInstance(self.base.updated_at, datetime.datetime, "Not a datetime object")
 
     def test_str(self):
         self.assertIsInstance(self.base.__str__(), str)
+        self.assertRegex(self.base.__str__(), "^\[BaseModel\] (.*) {(.*)}$")
 
     def test_save(self):
+        time.sleep(0.3)
         self.base.save()
         self.assertNotEqual(self.base.created_at, self.base.updated_at)
 
